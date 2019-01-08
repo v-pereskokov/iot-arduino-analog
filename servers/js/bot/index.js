d3adc7f0a35d7be70fd79efb5745b49d5e655fa8
vladpereskokov <vladislavpereskokov@yandex.ru>
2019-01-08 21:09:41 +0300

    add personal chat id

--- servers/js/bot/index.js
+++ servers/js/bot/index.js
@@ -3,4 +3,5 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TG_TOKEN_Barnum;
 const bot = new TelegramBot(token, {polling: true});
 
+// VP_chat: 75024699
 bot.on('message', (msg) => bot.sendMessage(msg.chat.id, 'Добрый день! Ждите уведомлений от меня...'));
