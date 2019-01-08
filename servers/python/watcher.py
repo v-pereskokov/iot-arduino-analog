142ce35d33ff14bc980e434e1cea39752a34a759
vladpereskokov <vladislavpereskokov@yandex.ru>
2019-01-08 21:22:08 +0300

    some refactor

--- servers/python/watcher.py
+++ servers/python/watcher.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 			if "close" in status.lower():
 				requests.get("http://192.168.1.153:5000/turn_motor")
 		elif "-" in temperature:
-		    requests.get("https://api.telegram.org/bot{}/sendMessage?text=В доме слишком холодно! Температура поднимается до допустимой Выход открыт.&chat_id=75024699".format(TOKEN))
+		    requests.get("https://api.telegram.org/bot{}/sendMessage?text=В доме слишком холодно! Температура поднимается до допустимой. Выход открыт.&chat_id=75024699".format(TOKEN))
 			requests.get("http://192.168.1.153:5000/set_lcd_text?text=Cold! Open moto.")
 
 			status = requests.get("http://192.168.1.153:5000/status_motor")
